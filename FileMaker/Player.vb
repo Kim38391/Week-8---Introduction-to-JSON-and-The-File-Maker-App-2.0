fef6d92727af8e7b829ed6d0debd4d62a7b7d9e9
Kim38391 <kim38391@gmail.com>
2026-08-05 19:52:12 -0700

    Add files via upload

---
+++ FileMaker/Player.vb
@@ -0,0 +1,13 @@
+﻿Public Class Player
+
+
+
+    Public Property name As String
+
+    Public Property age As String
+End Class
+Public Class Project
+    Public Property ProjectName As String
+    Public Property FullDirectory As String
+    Public Property LastSaved As String
+End Class
